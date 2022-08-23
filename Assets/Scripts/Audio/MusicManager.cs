7ecd677ee3b42c1743204f02d87f7c2471b2a390
Orangehawk <48203578+Orangehawk@users.noreply.github.com>
2022-08-23 16:43:10 +0800

    Made music pause when not in play state

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    public static MusicManager instance;
+
     [SerializeField]
     AudioSource audioSource;
 
-    // Start is called before the first frame update
-    void Start()
+	bool paused;
+    GameManager gameManager;
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Debug.LogWarning("Duplicate MusicManager!");
+			Destroy(this);
+		}
+		else
+		{
+			instance = this;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+        gameManager = GameManager.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (audioSource.isPlaying == false)
+		if (gameManager.GetState() == GameManager.State.Playing)
 		{
-            Debug.Log("Music ended, restarting music");
-            audioSource.Play();
+			if (paused)
+			{
+				audioSource.UnPause();
+				paused = false;
+			}
+			else if (audioSource.isPlaying == false && !paused)
+			{
+				Debug.Log("Music ended, restarting music");
+				audioSource.Play();
+				paused = false;
+			}
+		}
+		else
+		{
+			audioSource.Pause();
+			paused = true;
 		}
     }
 }
